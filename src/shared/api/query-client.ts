ac71a408bfaee580888986c9ba023ed96fe8f1c3
sadetskayfu <goblin_1444@mail.ru>
2025-10-16 02:59:29 +0300

    loh2

--- src/shared/api/query-client.ts
+++ src/shared/api/query-client.ts
@@ -1,9 +1,9 @@
-import { QueryClient } from "@tanstack/react-query";
+// import { QueryClient } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 5 * 60 * 1000
-        }
-    }
-})
+// export const queryClient = new QueryClient({
+//     defaultOptions: {
+//         queries: {
+//             staleTime: 5 * 60 * 1000
+//         }
+//     }
+// })
