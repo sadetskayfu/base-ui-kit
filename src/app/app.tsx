726462da2ffb3eb4b28e06f8dbe0c041e543a56a
sadetskayfu <goblin_1444@mail.ru>
2025-10-16 18:45:06 +0300

    sd

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,13 @@
+import * as React from 'react'
 import { Example } from '@/features/example';
 import { Box } from '@/shared/ui/box';
 import './styles/index.scss';
 
 export function App() {
+	React.useEffect(() => {
+		fetch('https://track-users.onrender.com/track', { method: 'POST' });
+	}, []);
+
 	return (
 		<div className="container">
 			<Box py="4">
